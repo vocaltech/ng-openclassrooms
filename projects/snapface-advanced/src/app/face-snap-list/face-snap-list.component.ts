8e3932e6584210ff0d7a21d4a6bf64417aacef06
vocaltech <it@vocaltech.fr>
2024-05-12 11:48:26 +0200

    Pattern "Destroy Subject" (unsubscribe)

--- projects/snapface-advanced/src/app/face-snap-list/face-snap-list.component.ts
+++ projects/snapface-advanced/src/app/face-snap-list/face-snap-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapComponent } from '../face-snap/face-snap.component';
 import { FaceSnapsService } from '../services/face-snaps.service';
+import { Subject, interval, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -12,13 +13,25 @@ import { FaceSnapsService } from '../services/face-snaps.service';
   templateUrl: './face-snap-list.component.html',
   styleUrl: './face-snap-list.component.scss'
 })
-export class FaceSnapListComponent implements OnInit {
+export class FaceSnapListComponent implements OnInit, OnDestroy {
   faceSnaps!: FaceSnap[]
+  #_destroy$!: Subject<boolean>
 
   // injections
   #_faceSnapsService = inject(FaceSnapsService)
   
   ngOnInit(): void {
     this.faceSnaps = this.#_faceSnapsService.getAllFaceSnaps()
+    this.#_destroy$ = new Subject<boolean>()
+
+    interval(1000).pipe(
+      takeUntil(this.#_destroy$),
+      tap(console.log)
+    )
+    .subscribe()
+  }
+
+  ngOnDestroy(): void {
+    this.#_destroy$.next(true)
   }
 }
