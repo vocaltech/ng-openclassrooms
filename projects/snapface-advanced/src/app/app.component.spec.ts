8a07df57c22fd9a529d22b1ca7434d021ea4e5c6
vocaltech <it@vocaltech.fr>
2024-05-12 10:28:28 +0200

    Clean app.component.spec.ts

--- projects/snapface-advanced/src/app/app.component.spec.ts
+++ projects/snapface-advanced/src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have the 'snapface-advanced' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('snapface-advanced');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, snapface-advanced');
-  });
 });
