ad37a115944a262c04456dd3dcd1aed80e92e73e
vocaltech <it@vocaltech.fr>
2024-05-12 10:25:02 +0200

    Register fr locale

--- projects/snapface-advanced/src/main.ts
+++ projects/snapface-advanced/src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { registerLocaleData } from '@angular/common';
+import * as localeFr from '@angular/common/locales/fr'
+
+registerLocaleData(localeFr.default)
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
