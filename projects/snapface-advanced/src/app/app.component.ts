2610344b272480ae21de41a37a5a3de6619cbcbf
vocaltech <it@vocaltech.fr>
2024-05-12 10:29:19 +0200

    Clean app.component.ts

--- projects/snapface-advanced/src/app/app.component.ts
+++ projects/snapface-advanced/src/app/app.component.ts
@@ -1,46 +1,19 @@
-import { AsyncPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Observable, interval, of } from 'rxjs';
-import { concatMap, delay, exhaustMap, filter, map, mergeMap, switchMap, take, tap } from 'rxjs/operators'
+import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    AsyncPipe
+    FaceSnapListComponent,
+    HeaderComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-
-  redTrainsCalled = 0;
-  yellowTrainsCalled = 0;
-
-  ngOnInit() {
-    interval(500).pipe(
-      take(10),
-      map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
-      tap(color => console.log(`${new Date().toLocaleTimeString()}: La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
-      switchMap(color => this.getTrainObservable$(color)),
-      tap(train => console.log(`${new Date().toLocaleTimeString()}: Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
-    ).subscribe();
-  }
-
-
-  getTrainObservable$(color: 'rouge' | 'jaune') {
-    const isRedTrain = color === 'rouge';
-    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
-    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
-    console.log(`${new Date().toLocaleTimeString()}: Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
-    return of({ color, trainIndex }).pipe(
-      delay(isRedTrain ? 2000 : 3000)
-    );
-  }
-
-  translateColor(color: 'rouge' | 'jaune') {
-    return color === 'rouge' ? 'red' : 'yellow';
-  }
+  ngOnInit(): void {}
 }
