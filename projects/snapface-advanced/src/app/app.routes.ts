ca2bf69d07f1a8047ec1bf4109188a0e325bd9d9
vocaltech <it@vocaltech.fr>
2024-05-14 23:12:32 +0200

    Add create_facesnap path

--- projects/snapface-advanced/src/app/app.routes.ts
+++ projects/snapface-advanced/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { SingleFaceSnapComponent } from './single-face-snap/single-face-snap.component';
+import { NewFaceSnapComponent } from './new-face-snap/new-face-snap.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
     { path: 'facesnaps', component: FaceSnapListComponent },
-    { path: 'facesnaps/:id', component: SingleFaceSnapComponent }
+    { path: 'facesnaps/:id', component: SingleFaceSnapComponent },
+    { path: 'create_facesnap', component: NewFaceSnapComponent }
 ];
