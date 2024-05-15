62cc186864c5dd9a4c93004f53c474decf9328f1
vocaltech <it@vocaltech.fr>
2024-05-15 22:43:58 +0200

    Add faceSnapPreview$ observable

--- projects/snapface-advanced/src/app/new-face-snap/new-face-snap.component.ts
+++ projects/snapface-advanced/src/app/new-face-snap/new-face-snap.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Observable, map } from 'rxjs';
+import { FaceSnap } from '../models/face-snap.model';
+import { AsyncPipe, DatePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-new-face-snap',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AsyncPipe,
+    UpperCasePipe,
+    DatePipe
   ],
   templateUrl: './new-face-snap.component.html',
   styleUrl: './new-face-snap.component.scss'
@@ -14,14 +20,24 @@ export class NewFaceSnapComponent implements OnInit {
   #_formBuilder = inject(FormBuilder)
 
   snapForm!: FormGroup
+  faceSnapPreview$!: Observable<FaceSnap> 
 
   ngOnInit(): void {
     this.snapForm = this.#_formBuilder.group({
       title: [null],
       description: [null],
-      imageUrl: [null],
+      imgUrl: [null],
       location: [null]
     })
+
+    this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
+      map<any, FaceSnap>(formValue => ({
+        ...formValue,
+        snaps: 0,
+        createdDate: new Date(),
+        id: 0
+      }))
+    )
   }
 
   onSubmitForm(): void {
