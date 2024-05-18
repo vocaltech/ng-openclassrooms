3ce9e06b43dfb157f98051600d1dce73f687f2a6
vocaltech <it@vocaltech.fr>
2024-05-18 23:50:01 +0200

    Update with Observable<FaceSnap[]>

--- projects/snapface-advanced/src/app/face-snap-list/face-snap-list.component.ts
+++ projects/snapface-advanced/src/app/face-snap-list/face-snap-list.component.ts
@@ -2,26 +2,29 @@ import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapComponent } from '../face-snap/face-snap.component';
 import { FaceSnapsService } from '../services/face-snaps.service';
-import { Subject, interval, takeUntil, tap } from 'rxjs';
+import { Observable, Subject, interval, takeUntil, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-face-snap-list',
   standalone: true,
   imports: [
-    FaceSnapComponent
+    FaceSnapComponent,
+    AsyncPipe
   ],
   templateUrl: './face-snap-list.component.html',
   styleUrl: './face-snap-list.component.scss'
 })
 export class FaceSnapListComponent implements OnInit, OnDestroy {
-  faceSnaps!: FaceSnap[]
+  faceSnaps$!: Observable<FaceSnap[]>
   #_destroy$!: Subject<boolean>
 
   // injections
   #_faceSnapsService = inject(FaceSnapsService)
   
   ngOnInit(): void {
-    this.faceSnaps = this.#_faceSnapsService.getAllFaceSnaps()
+    this.faceSnaps$ = this.#_faceSnapsService.getAllFaceSnaps()
+
     this.#_destroy$ = new Subject<boolean>()
 
     interval(1000).pipe(
