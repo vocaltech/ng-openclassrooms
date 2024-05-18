0da70269284ee8b81e97d58e13abf35830559887
vocaltech <it@vocaltech.fr>
2024-05-18 23:54:04 +0200

    Refactor onSnapBtnClick() with pipe()

--- projects/snapface-advanced/src/app/single-face-snap/single-face-snap.component.ts
+++ projects/snapface-advanced/src/app/single-face-snap/single-face-snap.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { FaceSnap } from '../models/face-snap.model';
 import { FaceSnapAction, FaceSnapsService } from '../services/face-snaps.service';
-import { DatePipe, NgClass, NgStyle, UpperCasePipe } from '@angular/common';
+import { AsyncPipe, DatePipe, NgClass, NgStyle, UpperCasePipe } from '@angular/common';
+import { Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-single-face-snap',
   standalone: true,
   imports: [
+    AsyncPipe,
     UpperCasePipe,
     DatePipe,
     NgClass,
@@ -18,10 +20,9 @@ import { DatePipe, NgClass, NgStyle, UpperCasePipe } from '@angular/common';
   styleUrl: './single-face-snap.component.scss'
 })
 export class SingleFaceSnapComponent implements OnInit {
-  faceSnap!: FaceSnap
+  faceSnap$!: Observable<FaceSnap>
   isLiked: boolean = false
   btnLikedText: string = "Like"
-  snapStyle!: object
 
   // injections
   #_route: ActivatedRoute = inject(ActivatedRoute)
@@ -29,23 +30,34 @@ export class SingleFaceSnapComponent implements OnInit {
 
   ngOnInit(): void {
     const faceSnapId = +this.#_route.snapshot.params['id']
-    this.faceSnap = this.#_faceSnapsService.getFaceSnapById(faceSnapId)!
-
-    this.snapStyle = {
-      'margin-left':'8px', 
-      'color': 'rgb(0, ' + this?.faceSnap?.snaps + ' ,0)'
-    }
+    this.faceSnap$ = this.#_faceSnapsService.getFaceSnapById(faceSnapId)!
   }
 
-  onSnapBtnClick = () => {
+  snapStyle(totalSnaps: number) {
+    return {
+      'margin-left':'8px', 
+      'color': 'rgb(0, ' + totalSnaps + ' , 0)'
+    }
+
+  }
+
+  onSnapBtnClick = (faceSnapId: number) => {
     this.isLiked = !this.isLiked
 
     if (this.isLiked) {
-      this.btnLikedText = 'Dislike!'
-      this.#_faceSnapsService.updateFaceSnapTotalById(this.faceSnap.id, FaceSnapAction.SNAP)
+       this.faceSnap$ = this.#_faceSnapsService.updateFaceSnapTotalById(faceSnapId, FaceSnapAction.SNAP)
+       .pipe(
+        tap(() => {
+          this.btnLikedText = 'Unlike!'
+        })
+       )
     } else {
-      this.btnLikedText = 'Like!'
-      this.#_faceSnapsService.updateFaceSnapTotalById(this.faceSnap.id, FaceSnapAction.UNSNAP)
+      this.faceSnap$= this.#_faceSnapsService.updateFaceSnapTotalById(faceSnapId, FaceSnapAction.UNSNAP)
+      .pipe(
+        tap(() => {
+          this.btnLikedText = 'Like!'
+        })
+      )
     }
   }
 }
