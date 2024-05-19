31d6916597a0edb5b6173cc199db4e2392d334f4
vocaltech <it@vocaltech.fr>
2024-05-19 15:18:05 +0200

    Update imports

--- projects/snapface-advanced/src/app/app.config.ts
+++ projects/snapface-advanced/src/app/app.config.ts
@@ -1,9 +1,8 @@
 import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './interceptors/auth.interceptor';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
