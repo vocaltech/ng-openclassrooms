8550323e65cf4ab6b1a132d3df2ddd7fba9e6572
vocaltech <it@vocaltech.fr>
2024-05-19 12:33:49 +0200

    Remove test

--- projects/snapface-advanced/src/app/interceptors/auth.interceptor.ts
+++ projects/snapface-advanced/src/app/interceptors/auth.interceptor.ts
@@ -6,18 +6,16 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService)
   const authToken = authService.getToken()
 
+  console.log(`authToken: ${authToken}`)
+
   // clone the request and add the authorization header
   let authReq: HttpRequest<any>
 
-  if (authToken) {
-    authReq = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${authToken}`,
-      }
-    })
-  } else {
-    authReq = req.clone()
-  }
+  authReq = req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${authToken}`
+    }
+  })
 
   // Pass the cloned request with the updated header to the next handler
   return next(authReq);
