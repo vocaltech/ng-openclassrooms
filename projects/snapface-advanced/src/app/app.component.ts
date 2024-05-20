882d640fd72e5f78cd0c234f5eb55927aabda2ad
vocaltech <it@vocaltech.fr>
2024-05-20 15:24:00 +0200

    Add FaceSnapsModule / remove FaceSnapListComponent

--- projects/snapface-advanced/src/app/app.component.ts
+++ projects/snapface-advanced/src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 import { CoreModule } from './core/core.module';
+import { FaceSnapsModule } from './face-snaps/face-snaps.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    FaceSnapListComponent,
-    CoreModule
+    CoreModule,
+    FaceSnapsModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
