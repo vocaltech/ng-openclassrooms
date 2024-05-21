ef31afea3e3a108a6837426b460c5f05ef2eaccb
vocaltech <it@vocaltech.fr>
2024-05-21 19:04:06 +0200

    Remove unused components

--- projects/snapface-advanced/src/app/app.routes.ts
+++ projects/snapface-advanced/src/app/app.routes.ts
@@ -1,12 +1,8 @@
 import { Routes } from '@angular/router';
-import { FaceSnapListComponent } from './face-snaps/components/face-snap-list/face-snap-list.component';
 import { LandingPageComponent } from './landing-page/components/landing-page/landing-page.component';
-import { SingleFaceSnapComponent } from './face-snaps/components/single-face-snap/single-face-snap.component';
-import { NewFaceSnapComponent } from './face-snaps/components/new-face-snap/new-face-snap.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
-    { path: 'facesnaps', component: FaceSnapListComponent },
-    { path: 'facesnaps/:id', component: SingleFaceSnapComponent },
-    { path: 'create', component: NewFaceSnapComponent }
+    { path: 'facesnaps', loadChildren: () => import('./face-snaps/face-snaps.module').then(m => m.FaceSnapsModule) }
 ];
+
