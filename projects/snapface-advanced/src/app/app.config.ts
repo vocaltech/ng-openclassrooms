bc15e29a6940062bdb2d4a789505a21112d7069f
vocaltech <it@vocaltech.fr>
2024-05-23 23:46:54 +0200

    Add provideRouter for loginRoutes

--- projects/snapface-advanced/src/app/app.config.ts
+++ projects/snapface-advanced/src/app/app.config.ts
@@ -1,12 +1,14 @@
 import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { loginRoutes } from './auth/auth.routes'
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideRouter(loginRoutes),
     {
       provide: LOCALE_ID,
       useValue: 'fr_FR'
