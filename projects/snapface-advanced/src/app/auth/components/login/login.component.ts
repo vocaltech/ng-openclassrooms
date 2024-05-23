d72fe04052585219ecf07f633ea03f0a6f5800d7
vocaltech <it@vocaltech.fr>
2024-05-23 23:52:33 +0200

    Implement  onSubmitForm() using AuthService

--- projects/snapface-advanced/src/app/auth/components/login/login.component.ts
+++ projects/snapface-advanced/src/app/auth/components/login/login.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../core/services/auth.service';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  #authService = inject(AuthService)
+
+  userEmail: string = ''
+  userPassword: string = ''
+
+  onSubmitForm = (form: NgForm) => {
+    console.log(form.value)
+    this.#authService.login(form.value.email, form.value.password)
+  }
 
 }
