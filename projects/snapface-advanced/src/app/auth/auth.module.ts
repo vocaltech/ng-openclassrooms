0047505a83a53522c353be9fe3a79cde906337c8
vocaltech <it@vocaltech.fr>
2024-05-23 23:49:22 +0200

    Use loginRoutes for provideRouter

--- projects/snapface-advanced/src/app/auth/auth.module.ts
+++ projects/snapface-advanced/src/app/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { provideRouter } from '@angular/router';
-import { routes } from './auth.routes';
+
 import { LoginComponent } from './components/login/login.component';
+import { provideRouter } from '@angular/router';
+import { loginRoutes } from './auth.routes';
 
 @NgModule({
   declarations: [],
@@ -10,8 +11,8 @@ import { LoginComponent } from './components/login/login.component';
     CommonModule,
     LoginComponent
   ],
-  providers: [
-    provideRouter(routes)
+  providers:  [
+    provideRouter(loginRoutes)
   ]
 })
 export class AuthModule { }
