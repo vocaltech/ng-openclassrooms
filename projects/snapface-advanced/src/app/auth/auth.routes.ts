bbecf7ec6990fce01af14833dc80313a291751f2
vocaltech <it@vocaltech.fr>
2024-05-23 23:50:14 +0200

    Use loginRoutes variable

--- projects/snapface-advanced/src/app/auth/auth.routes.ts
+++ projects/snapface-advanced/src/app/auth/auth.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from "@angular/router";
 import { LoginComponent } from "./components/login/login.component";
 
-export const routes: Routes = [
-    { path: 'login', component: LoginComponent }
+export const loginRoutes: Routes = [
+    { path: 'auth/login', component: LoginComponent }
 ]
