6820e796299d3df3eb1bc568c0ac8751d93a59d8
vocaltech <it@vocaltech.fr>
2024-05-23 23:47:39 +0200

    Remove login path from app.routes.ts

--- projects/snapface-advanced/src/app/app.routes.ts
+++ projects/snapface-advanced/src/app/app.routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/components/landing-page/landing-page.component';
-import { LoginComponent } from './auth/components/login/login.component';
 
 export const routes: Routes = [
     { path: '', component: LandingPageComponent },
     { path: 'facesnaps', loadChildren: () => import('./face-snaps/face-snaps.module').then(m => m.FaceSnapsModule) },
-    { path: 'login', component: LoginComponent }
 ];
 
