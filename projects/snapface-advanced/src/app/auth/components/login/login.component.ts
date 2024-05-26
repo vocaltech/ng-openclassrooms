c15268c7e0b2916a4d1a0e5c0fbf4fcf53a0ede7
vocaltech <it@vocaltech.fr>
2024-05-26 12:37:03 +0200

    Replace onSubmitForm() with onLogin()

--- projects/snapface-advanced/src/app/auth/components/login/login.component.ts
+++ projects/snapface-advanced/src/app/auth/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../../core/services/auth.service';
 import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,13 +14,14 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class LoginComponent {
   #authService = inject(AuthService)
+  #router = inject(Router)
 
   userEmail: string = ''
   userPassword: string = ''
 
-  onSubmitForm = (form: NgForm) => {
+  onLogin = (form: NgForm) => {
     console.log(form.value)
     this.#authService.login(form.value.email, form.value.password)
+    this.#router.navigateByUrl('facesnaps')
   }
-
 }
