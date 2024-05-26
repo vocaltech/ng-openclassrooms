feed6fffc3e216fa7630b37a10969c1afae5a8be
vocaltech <it@vocaltech.fr>
2024-05-26 12:37:31 +0200

    Add canActivate to Routes

--- projects/snapface-advanced/src/app/face-snaps/face-snaps.routes.ts
+++ projects/snapface-advanced/src/app/face-snaps/face-snaps.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from "@angular/router";
 import { FaceSnapListComponent } from "./components/face-snap-list/face-snap-list.component";
 import { SingleFaceSnapComponent } from "./components/single-face-snap/single-face-snap.component";
 import { NewFaceSnapComponent } from "./components/new-face-snap/new-face-snap.component";
+import { AuthGuard } from "../core/guards/auth.guard";
 
 export const routes: Routes = [
-    { path: '', component: FaceSnapListComponent },
-    { path: 'create', component: NewFaceSnapComponent },
-    { path: ':id', component: SingleFaceSnapComponent }
+    { path: '', component: FaceSnapListComponent, canActivate: [AuthGuard] },
+    { path: 'create', component: NewFaceSnapComponent, canActivate: [AuthGuard] },
+    { path: ':id', component: SingleFaceSnapComponent, canActivate: [AuthGuard] }
 ];
